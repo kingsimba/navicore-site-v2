0678df7fe1440768f4d1939d6b3391af2a7c783d
kingsimba <kingsimba@mapbar.com>
2020-02-13 00:24:37 +0800

    add testing code

--- src/app/jnavicore/jnavicore.component.ts
+++ src/app/jnavicore/jnavicore.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Hero {
+  id: number;
+  name: string;
+}
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-jnavicore',
   templateUrl: './jnavicore.component.html',
@@ -7,9 +25,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JnavicoreComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
