de22a46391e732110f5b968dd495ca542fda2f42
kingsimba <kingsimba@mapbar.com>
2020-02-13 22:31:42 +0800

    update home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -47,6 +48,7 @@ const ROUTES: Routes = [
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    NzIconModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
