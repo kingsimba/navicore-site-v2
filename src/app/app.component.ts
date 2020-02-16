42971d46566898f8f2305b57a6db56ce43cbed7e
kingsimba <kingsimba@mapbar.com>
2020-02-16 12:12:42 +0800

    for standalone document, hide header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -7,11 +8,15 @@ import { HostListener } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    constructor() {
+    constructor(private router: Router) {
 
     }
 
     // tslint:disable-next-line: use-lifecycle-interface
     ngOnInit() {
+    }
+
+    isStandaloneDocument() {
+        return this.router.url.endsWith('.html') || this.router.url.endsWith('htm');
     }
 }
