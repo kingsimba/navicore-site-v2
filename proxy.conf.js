1f054e14d516cc5dfe3f4432b6d82b6916accfa5
kingsimba <kingsimba@mapbar.com>
2020-02-25 10:37:40 +0800

    limit doc width

--- proxy.conf.js
+++ proxy.conf.js
@@ -3,7 +3,7 @@ const PROXY_CONFIG = [
       context: [
           "/api"
       ],
-      target: "http://dal.navicore.cn:9080",
+      target: "http://navicore.mapbar.com:9080",
       secure: false
   }
 ]
