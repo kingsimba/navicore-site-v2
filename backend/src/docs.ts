78b15a8b2c7dc86b1109cfe5f0dc98ce0defac37
kingsimba <kingsimba@mapbar.com>
2020-04-06 23:40:44 +0800

    simplify

--- backend/src/docs.ts
+++ backend/src/docs.ts
@@ -6,10 +6,8 @@ import { JSDOM } from 'jsdom'
 export const docsRouter = express.Router();
 
 function authMiddleware(req: express.Request, res: express.Response, next: () => void) {
-    const user = userManager.verifyRequestAndRefreshCookie(req, res);
-    if (!user) {
-        res.status(401);
-        res.send({ status: 401, message: 'Please login first' });
+    if (!userManager.verifyRequestAndRefreshCookie(req, res)) {
+        res.status(401).send({ status: 401, message: 'Please login first' });
     } else {
         next();
     }
@@ -27,28 +25,20 @@ async function getDocName(dirName: string) : Promise<string> {
 
 // list documents under 'docs' folder
 async function docListMiddleware(req: express.Request, res: express.Response, next: () => void) {
-    if (req.url == '/') {
+    if (req.url === '/') {
         try {
             const docs = [];
             const dirs = await fs.readdir('docs');
             for (const d of dirs) {
-                const st = await fs.stat(`docs/${d}`);
-                if (st.isDirectory()) {
-                    const name = await getDocName(d);
-                    if (name) {
-                        docs.push({ name: name, path: d });
-                    }
+                const name = await getDocName(d);
+                if (name) {
+                    docs.push({ name: name, path: d });
                 }
             };
 
-            res.status(200);
-            res.send({
-                status: 200,
-                docs: docs
-            });
+            res.send({ status: 200, docs: docs });
         } catch (error) {
-            res.status(500);
-            res.send({ status: 500, message: error });
+            res.status(500).send({ status: 500, message: error });
         }
     } else {
         next();
@@ -59,6 +49,5 @@ docsRouter.use(authMiddleware);
 docsRouter.use('/', docListMiddleware); // serve static files
 docsRouter.use('/', express.static('docs')); // serve static files
 docsRouter.use(function (req, res) {    // custom 404 page
-    res.status(404);
-    res.send('404: Page not Found');
+    res.status(404).send('404: Page not Found');
 });
