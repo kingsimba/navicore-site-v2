e5fb723f15f06e9a95934165391ba36e00086805
kingsimba <kingsimba@mapbar.com>
2020-04-16 15:09:52 +0800

    support /nc/v1/ca

--- backend/src/index.spec.ts
+++ backend/src/index.spec.ts
@@ -6,3 +6,12 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe('/api/v1/ca', async () => {
+
+    it('return ca information', async () => {
+        const res = await chai.request(app)
+            .get('/api/v1/ca');
+        expect(res).to.have.status(200);
+        expect(res.body.daysLeft).is.a('number');
+    });
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { authRouter } from "./auth";
 import { docsRouter } from "./docs";
 import { globalOptions } from "./global-options";
 import { tokenManager as tokenManager } from './token-manager'
+var checkCertExpiration = require('check-cert-expiration');
 
 export const app = express();
 
@@ -20,6 +21,15 @@ app.use(CookieParser());
 // Sub-routers
 app.use('/api/v1/docs', docsRouter);
 app.use('/api/v1/auth', authRouter);
+app.get('/api/v1/ca', async function (req, res) {
+    try {
+        const { daysLeft, host, port } = await checkCertExpiration('navicore.cn');
+        res.send({ host, port, daysLeft });
+    } catch (err) {
+        res.status(500).send({ status: 500, err });
+    }
+    res.status(404).send('404: Page not Found');
+});
 
 // start the Express server
 app.listen(port, () => {
